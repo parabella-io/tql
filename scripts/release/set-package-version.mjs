b6a0f210a394014b0f5cec43a2fa18df1556fa77
Declan Price <declanprice1@gmail.com>
2026-05-07 16:26:41 +1000

    fix: release 1.1.1

--- scripts/release/set-package-version.mjs
+++ scripts/release/set-package-version.mjs
@@ -2,7 +2,16 @@ import { readFile, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
 const version = process.argv[2];
-const packages = ['packages/server/package.json', 'packages/client/package.json'];
+const packages = [
+  'apps/api/package.json',
+  'apps/app/package.json',
+  'apps/docs/package.json',
+  'apps/e2e/package.json',
+  'packages/server/package.json',
+  'packages/client/package.json',
+  'packages/ts-config/package.json',
+  'package.json',
+];
 
 if (!version) {
   throw new Error('Usage: pnpm release:version <version>');
